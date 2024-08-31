553ca307149fc58216ac2a55d08a0bfe148b715a
verakissyou <veronicaciuca17@gmail.com>
2024-09-01 02:00:41 +0300

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,8 +14,9 @@ cart.forEach((cartItem) => {
             matchingProduct = product;
         }
     });
+
     cartSummaryHTML += 
-    `<div class="cart-item-container">
+    `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
         <div class="cart-item-details-grid">
@@ -88,5 +89,8 @@ document.querySelectorAll('.js-delete-link').forEach((deleteLink) => {
     deleteLink.addEventListener('click', (event) => {
        const productId = deleteLink.dataset.productId;
        removeFromCart(productId);
+
+    const container =  document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
     });
 });
