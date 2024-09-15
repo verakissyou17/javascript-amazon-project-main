d0559521b69571254f1461925f2371fbb6691522
verakissyou <veronicaciuca17@gmail.com>
2024-09-15 20:46:28 +0300

    Style the console in the simple tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,35 +1,61 @@
 import {formatCurrency} from '../../scripts/utils/money.js';
-//Test suite for formatCurrency
-console.log(' test suite: formatCurrency');
-//Test case 1 - Basic case
-console.log('converts cents into dollars');
-if (formatCurrency(2095) === '20.95') {
-    console.log('Passed');
-} else {
-    console.log('Failed');
-};
 
-//Test case 2 - Edge case
-console.log('works with 0');
-if (formatCurrency(0) === '0.00') {
-    console.log('Passed');
-} else {
-    console.log('Failed');
-};
+// Test suite for formatCurrency
+console.log("test suite: formatCurrency");
 
-//Test case 3 - Edge case
-console.log('rounds up to the nearest cent');
-if (formatCurrency(2000.5) === '20.01') {
-    console.log('Passed');
+//Test 1 - base case
+console.log("converts cents to dolllars");
+if (formatCurrency(2095) === "20.95") {
+  console.log(
+    "%cPASSED",
+    "color: green; font-style: italic; font-weight: bold; font-size: 18px"
+  );
 } else {
-    console.log('Failed');
-};
+  console.log(
+    "%cFAILED",
+    "color: red; font-style: italic; font-weight: bold; font-size: 18px"
+  );
+}
 
-//Test case 4 - Edge case
-console.log('rounds down to the nearest cent');
-if (formatCurrency(2000.04) === '20.00') {
-    console.log('Passed');
+//Test 2 - edge case
+console.log("works with 0");
+if (formatCurrency(0) === "0.00") {
+  console.log(
+    "%cPASSED",
+    "color: green; font-style: italic; font-weight: bold; font-size: 18px"
+  );
 } else {
-    console.log('Failed');
-};
+  console.log(
+    "%cFAILED",
+    "color: red;font-style: italic; font-weight: bold; font-size: 18px"
+  );
+}
+
+//Test 3 - edge case
+console.log("rounds up to the nearest cent");
+if (formatCurrency(2000.5) === "20.01") {
+  console.log(
+    "%cPASSED",
+    "color: green; font-style: italic; font-weight: bold; font-size: 18px"
+  );
+} else {
+  console.log(
+    "%cFAILED",
+    "color: red; font-style: italic; font-weight: bold; font-size: 18px"
+  );
+}
+
+//Test 4 - edge case
+console.log("rounds down to the nearest cent");
+if (formatCurrency(2000.4) === "20.00") {
+  console.log(
+    "%cPASSED",
+    "color: green; font-style: italic; font-weight: bold; font-size: 18px"
+  );
+} else {
+  console.log(
+    "%cFAILED",
+    "color: red; font-style: italic; font-weight: bold; font-size: 18px"
+  );
+}
 
