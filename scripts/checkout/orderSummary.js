2bf037df9cba6104a803a451b8bc3b08bdc2fbce
verakissyou <veronicaciuca17@gmail.com>
2024-09-19 21:37:17 +0300

    Convert products into classes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -41,7 +41,7 @@ cart.forEach((cartItem) => {
                 ${matchingProduct.name}
                 </div>
                 <div class="product-price js-product-price-${matchingProduct.id}">
-                $${formatCurrency(matchingProduct.priceCents)}
+                ${matchingProduct.getPrice()}
                 </div>
                 <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                 <span> Quantity: 
