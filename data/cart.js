ad35e249101a3e5f9914971d190b145ece2a067c
verakissyou <veronicaciuca17@gmail.com>
2024-09-22 00:21:34 +0300

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -91,3 +91,15 @@ export function calculateCartQuantity() {
     matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
   };
+
+  export  function loadCart (fun) {
+    const xhr = new XMLHttpRequest();
+   
+     xhr.addEventListener('load', () => {
+      console.log(xhr.response)
+       fun();
+     });
+   
+    xhr.open('GET', 'https://supersimplebackend.dev/cart');
+    xhr.send();
+   }
--- data/products.js
+++ data/products.js
@@ -67,7 +67,7 @@ export  function loadProducts (fun) {
       }
       return  new Product(productDetails);
     });
-    
+    console.log('load products');
     fun();
   });
 
