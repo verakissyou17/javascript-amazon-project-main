9c91906df60067b03f71c12bf43eda9fbb7d6d24
verakissyou <veronicaciuca17@gmail.com>
2024-09-21 23:12:27 +0300

    Load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe("test suite: renderOrderSummary", () => {
     const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
@@ -7,6 +8,12 @@ describe("test suite: renderOrderSummary", () => {
     const productName1 = 'Black and Gray Athletic Cotton Socks - 6 Pairs';
     const productName2 = 'Intermediate Size Basketball';
     const deliveryOption3 = '3';
+
+    beforeAll((done) => {
+      loadProducts(() => {
+        done();
+      });
+    })
 
     beforeEach(() => {
         spyOn(localStorage, "setItem");
--- tests/data/productsTest.js
+++ tests/data/productsTest.js
@@ -1,12 +1,19 @@
-import {getProduct} from '../../data/products.js';
+import {getProduct, loadProducts} from '../../data/products.js';
 
 describe('test suite: getProduct', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    
+    beforeAll((done) => {
+        loadProducts(() => {
+          done();
+        });
+      })
 
     it('should display information about the product', () => {
+        
         const product = getProduct(productId1);
-        expect(product.name).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
         expect(product.id).toEqual(productId1);
+        expect(product.name).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
         expect(product.image).toEqual('images/products/athletic-cotton-socks-6-pairs.jpg');
         expect(product.priceCents).toEqual(1090);
     });
